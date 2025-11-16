7d161598375aa1016a9e5accc51a324e5e15b866
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-11-16 15:45:10 +0100

    reset Footer e Body

--- components/AppFooter.jsx
+++ components/AppFooter.jsx
@@ -4,30 +4,6 @@ export default function AppFooter() {
     return (
         <>
 
-            <div className="d-flex bg-dark bg-gradient justify-content-between">
-                <nav className="navbar">
-                    <div className="container-fluid">
-                        <a className="navbar-brand text-light" href="#">
-                            Bootstrap
-                        </a>
-                    </div>
-                </nav>
-
-                <ul className="nav nav-pills nav-fill">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-
-                </ul>
-            </div>
-
-
 
 
 
